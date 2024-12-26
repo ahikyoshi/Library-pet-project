51ab38c8c3bd33421707862cf323722d81d2bb65
ahikyoshi <115409269+ahikyoshi@users.noreply.github.com>
2024-12-26 14:54:29 +0500

    Daily-211224
    Изменения:
    
    1. Добавлена форма предложения книг

--- src/pages/admin/library/new/utils.ts
+++ src/pages/admin/library/new/utils.ts
@@ -3,11 +3,17 @@ import { IHandleSubmit } from "./types";
 
 export const handleSubmit = ({
     event,
+    mode,
     setIsStatus,
     setIsLoading
 }: IHandleSubmit) => {
     event.preventDefault();
+
+    const form = event.currentTarget;
+    const inputs = form.querySelectorAll("input");
+
     const formData = new FormData(event.currentTarget);
+
     const title = String(formData.get("title")).trim();
     const author = String(formData.get("author")).trim();
     const cycle = {
@@ -19,7 +25,11 @@ export const handleSubmit = ({
         writtingDate: String(formData.get("writtingDate")).trim()
     };
 
-    if (!title || !author) {
+    if (Number(meta.writtingDate) < 1980 || Number(meta.writtingDate) > 2025) {
+        setIsStatus({
+            success: false,
+            message: "Ошибка при заполнении года написания"
+        });
         return;
     }
 
@@ -39,12 +49,8 @@ export const handleSubmit = ({
     })
         .then((res) => res.json())
         .then(({ success, message }: { success: boolean, message: string }) => {
-            if (success) {
-                window.location.href = "/admin/library";
-            } else {
-                setIsStatus({ success, message: message });
-                setIsLoading(false);
-            }
+            setIsStatus({ success, message: message });
+            setIsLoading(false);
         })
         .catch(() => {
             console.log("something went wrong");
@@ -54,4 +60,29 @@ export const handleSubmit = ({
             });
             setIsLoading(false);
         });
+
+    if (mode != "back") {
+        inputs.forEach((input) => {
+            if (
+                input.name === "title" ||
+                input.name === "description" ||
+                input.name === "writtingDate"
+            ) {
+                input.value = "";
+            }
+            if (input.name === "cycle_number") {
+                input.value = String(Number(input.value) + 1);
+            }
+        });
+    }
+    if (mode === "continue-author") {
+        inputs.forEach((inputs) => {
+            if (
+                inputs.name === "cycle_name" ||
+                inputs.name === "cycle_number"
+            ) {
+                inputs.value = "";
+            }
+        });
+    }
 };
